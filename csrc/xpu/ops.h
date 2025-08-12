ccc900bd31acb5ad6fb044aa860aeb895f7873b7
zofia <110436990+zufangzhu@users.noreply.github.com>
2025-08-12 11:21:26 +0800

    Add cache ops (#3)
    * add reshape_and_cache_flash_kernel
    
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    * add reshape_and_cache
    
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    * add benchmark and ut
    
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    * add vevtorization
    
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    * add ut and benchmark for reshape_and_cache
    
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    * refine test
    
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    * format by pre-commit
    
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    * update as review
    
    Signed-off-by: Zhu <zufang.zhu@intel.com>
    
    ---------
    
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>
    Signed-off-by: Zhu <zufang.zhu@intel.com>

--- csrc/xpu/ops.h
+++ csrc/xpu/ops.h
@@ -7,3 +7,16 @@ void rms_norm(torch::Tensor& out, torch::Tensor& input, torch::Tensor& weight,
 
 void fused_add_rms_norm(torch::Tensor& input, torch::Tensor& residual,
                         torch::Tensor& weight, double epsilon);
+
+void reshape_and_cache(torch::Tensor& key, torch::Tensor& value,
+                       torch::Tensor& key_cache, torch::Tensor& value_cache,
+                       torch::Tensor& slot_mapping,
+                       const std::string& kv_cache_dtype,
+                       torch::Tensor& k_scale, torch::Tensor& v_scale);
+
+void reshape_and_cache_flash(torch::Tensor& key, torch::Tensor& value,
+                             torch::Tensor& key_cache,
+                             torch::Tensor& value_cache,
+                             torch::Tensor& slot_mapping,
+                             const std::string& kv_cache_dtype,
+                             torch::Tensor& k_scale, torch::Tensor& v_scale);
